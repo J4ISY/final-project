02a66ba6402a3422711aa065d13edf592047aca9
Jakub Marciniak <kubamar02@gmail.com>
2023-04-24 12:49:44 +0200

    add template to site using react

---
+++ src/assets/scripts/template/Footer.jsx
@@ -0,0 +1,21 @@
+
+
+function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                <div className="footer__wrapper">
+                    <div className="footer-left">
+                        <p className="footer-text">&copy; 2023 Prawa zastrzeżone (Dane klienta). All Rights Reserved.</p>
+                    </div>
+                    <div className="footer-right">
+                        <a href="#" className="footer-links"><i className="fa-brands fa-square-facebook fa-2xl"></i></a>
+                        <a href="#" className="footer-links"><i className="fa-brands fa-instagram fa-2xl"></i></a>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/assets/scripts/template/Header.jsx
@@ -0,0 +1,31 @@
+
+function Header() {
+
+  return (
+    <>
+      <header className="header">
+        <div className="header__wrapper">
+          <nav className="nav-header nav-left">
+            <ul className='ul-header ul-header-left'>
+              <li className='li-header li-space-left'><a href="#" className="nav__link">Strona główna</a></li>
+              <li className='li-header'><a href="#" className="nav__link">Oferta</a></li>
+            </ul>
+          </nav>
+          <div className="logo">
+            <a href="#" className="logo__link">
+              <img src="https://via.placeholder.com/407x93" alt="" className="logo__img"/>
+            </a>
+          </div>
+          <nav className="nav-header nav-right">
+            <ul className='ul-header ul-header-right'>
+              <li className='li-header li-space-right'><a href="#" className="nav__link">Blog</a></li>
+              <li className='li-header'><a href="#" className="nav__link">Galeria</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
