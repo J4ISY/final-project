3d59e3f979c8750551acc730b2fff842b1675ec3
Jakub Marciniak <kubamar02@gmail.com>
2023-06-06 13:55:32 +0200

    add separated classes to tags

--- src/assets/scripts/components/Offer/OfferAll.jsx
+++ src/assets/scripts/components/Offer/OfferAll.jsx
@@ -8,35 +8,35 @@ function OfferAll() {
                 {/*    /!*<img src="/src/assets/images/imgUse/offer-banner.jpg" alt="" className="offer-header-img"/>*!/*/}
                 {/*    <div className="offer-header-img"></div>*/}
                 {/*</div>*/}
-                <div className="offer-wrapper">
+                <div className="offer-page-wrapper">
 
-                    <div className="offer-single offer-single-first">
-                        <div className="single-img-left single-img">
+                    <div className="offer-single-page offer-single-page-first">
+                        <div className="single-img-page-left single-page-img">
                             <img src="/src/assets/images/imgUse/maja-marciniak/image0bgc.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-left img-img"/>
+                                 alt="zdjęcie konsultacje indywidualne" className="img-left-page img-img-page"/>
                         </div>
-                        <div className="content-right content">
-                            <h6 className="cat">Trening mentalny</h6>
-                            <h3 className="offer-title">KONSUTLACJE INDYWIDUALNE</h3>
-                            <p className="offer-short-desc">Oferuję swoje wsparcie, doradztwo i pomoc w rozwiązaniu
+                        <div className="content-right content-page">
+                            <h6 className="cat-page">Trening mentalny</h6>
+                            <h3 className="offer-title-page">KONSUTLACJE INDYWIDUALNE</h3>
+                            <p className="offer-short-desc-page">Oferuję swoje wsparcie, doradztwo i pomoc w rozwiązaniu
                                 konkretnego problemu. Jeśli w swojej
                                 pracy trenera napotkałeś na wyzwanie, potrzebujesz merytorycznej podpowiedzi z zakresu
                                 psychologii, zarządzania zespołem – ta oferta jest dla Ciebie.</p>
-                            <div className="read-more">
-                                <Link to='/oferta/konsultacje-indywidualne' className="read-more-btn">Czytaj dalej</Link>
+                            <div className="read-more-page">
+                                <Link to='/oferta/konsultacje-indywidualne' className="read-more-page-btn">Czytaj dalej</Link>
                             </div>
                         </div>
                     </div>
 
-                    <div className="offer-single offer-single-second">
-                        <div className="single-img-right single-img">
+                    <div className="offer-single-page offer-single-page-second">
+                        <div className="single-img-page-right single-img-page">
                             <img src="/src/assets/images/imgUse/maja-marciniak/image6.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-right img-img"/>
+                                 alt="zdjęcie konsultacje indywidualne" className="img-right-page img-img-page"/>
                         </div>
-                        <div className="content-left content">
-                            <h6 className="cat">Trening mentalny</h6>
-                            <h3 className="offer-title">LEADERSHIP COACHING – droga do świadomego przywództwa.</h3>
-                            <p className="offer-short-desc">Zapraszam Cię do procesu coachingowego, podczas którego
+                        <div className="content-left-page content-page">
+                            <h6 className="cat-page">Trening mentalny</h6>
+                            <h3 className="offer-title-page">LEADERSHIP COACHING – droga do świadomego przywództwa.</h3>
+                            <p className="offer-short-desc-page">Zapraszam Cię do procesu coachingowego, podczas którego
                                 rozwiniesz i wzbogacisz swój styl
                                 trenerski. Jest to oferta skierowana do trenerów, którzy chcą świadomie i skutecznie
                                 prowadzić
@@ -44,55 +44,55 @@ function OfferAll() {
                                 ścieżka, która rozbudza i umacnia cechy przywódcze, pozwala czerpać satysfakcję z
                                 zarządzania
                                 ludźmi.</p>
-                            <div className="read-more">
-                                <Link to='/oferta/leadership-coaching' className="read-more-btn">Czytaj dalej</Link>
+                            <div className="read-more-page">
+                                <Link to='/oferta/leadership-coaching' className="read-more-page-btn">Czytaj dalej</Link>
                             </div>
                         </div>
-                        <div className="single-img-right single-img">
+                        <div className="single-img-right single-img-page">
                             <img src="/src/assets/images/imgUse/maja-marciniak/image6.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-right img-img"/>
+                                 alt="zdjęcie konsultacje indywidualne" className="img-right-page img-img-page"/>
                         </div>
                     </div>
 
-                    <div className="offer-single">
-                        <div className="single-img-left single-img">
+                    <div className="offer-single-page">
+                        <div className="single-img-left-page single-img-page">
                             <img src="/src/assets/images/imgUse/maja-marciniak/image8.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-left img-img"/>
+                                 alt="zdjęcie konsultacje indywidualne" className="img-left-page img-img-page"/>
                         </div>
-                        <div className="content-right content">
-                            <h6 className="cat">Trening mentalny</h6>
-                            <h3 className="offer-title">PROGRAM MENTORINGOWY PREMIUM</h3>
-                            <p className="offer-short-desc">Jest to oferta skierowana do trenerów pracujących z zespołami seniorskimi. Oferuję swoje
+                        <div className="content-right-page content-page">
+                            <h6 className="cat-page">Trening mentalny</h6>
+                            <h3 className="offer-title-page">PROGRAM MENTORINGOWY PREMIUM</h3>
+                            <p className="offer-short-desc-page">Jest to oferta skierowana do trenerów pracujących z zespołami seniorskimi. Oferuję swoje
                                 wsparcie i doradztwo w zakresie psychologii sportu, prowadzenia drużyny, przygotowania do
                                 rozgrywek, meczów i rozwiązywania bieżących problemów.</p>
-                            <div className="read-more">
-                                <Link to='/oferta/program-mentoringowy-premium' className="read-more-btn">Czytaj dalej</Link>
+                            <div className="read-more-page">
+                                <Link to='/oferta/program-mentoringowy-premium' className="read-more-page-btn">Czytaj dalej</Link>
                             </div>
                         </div>
                     </div>
 
-                    <div className="offer-single offer-single-second">
-                        <div className="single-img-right single-img">
-                            <img src="/src/assets/images/imgUse/maja-marciniak/image8.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-right img-img"/>
+                    <div className="offer-single-page offer-single-page-second">
+                        <div className="single-img-right-page single-img-page">
+                            <img src="/src/assets/images/imgUse/maja-marciniak/image7.jpeg"
+                                 alt="zdjęcie konsultacje indywidualne" className="img-right-page img-img-page"/>
                         </div>
-                        <div className="content-left content">
-                            <h6 className="cat">Online</h6>
-                            <h3 className="offer-title">WARSZTATY, EBOOKI, SZKOLENIA</h3>
-                            <p className="offer-short-desc">Już <span style={{fontWeight: 900}}>niedługo</span> pojawi się oferta umożliwiająca rozwijanie kompetencji miękkich. Pracujemy nad
+                        <div className="content-left-page content-page">
+                            <h6 className="cat-page">Online</h6>
+                            <h3 className="offer-title-page">WARSZTATY, EBOOKI, SZKOLENIA</h3>
+                            <p className="offer-short-desc-page">Już <span style={{fontWeight: 900}}>niedługo</span> pojawi się oferta umożliwiająca rozwijanie kompetencji miękkich. Pracujemy nad
                                 materiałami wspierającymi pracę trenerów w zakresie : komunikacji, budowania motywacji w
                                 zespole, zarządzania sytuacjami trudnymi, umiejętności udzielania informacji zwrotnej,
                                 zarządzania sztabem, radzenia sobie ze stresem i presją oraz skuteczną mową ciała trenera.
 
                                 Warsztaty i szkolenia będą prowadzone online, dzięki czemu każdy będzie mógł rozwijać się w
                                 dogodnym dla siebie czasie i mieć swobodny dostęp do materiałów.</p>
-                            <div className="read-more">
-                                {/*<Link to='/' className="read-more-btn">Czytaj dalej</Link>*/}
+                            <div className="read-more-page">
+                                {/*<Link to='/' className="read-more-page-btn">Czytaj dalej</Link>*/}
                             </div>
                         </div>
-                        <div className="single-img-right single-img">
+                        <div className="single-img-right-page single-img-page">
                             <img src="/src/assets/images/imgUse/maja-marciniak/image7.jpeg"
-                                 alt="zdjęcie konsultacje indywidualne" className="img-right img-img"/>
+                                 alt="zdjęcie konsultacje indywidualne" className="img-right-page img-img-page"/>
                         </div>
                     </div>
 
